c3d1f5cf38b5bbe86b7805fc1dbc8b397f9b2ea3
jbodur <jbodur@team.amocrm.com>
2022-09-13 23:39:31 +0300

    design system

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {renderRoutes} from '@/presentation/routes'
 
 import {HashRouter, Routes} from 'react-router-dom';
 
+import '@/presentation/styles/index.scss';
 
 function App() {
   return (
