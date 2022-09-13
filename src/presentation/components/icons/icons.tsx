f074f14920e03bbdbb2681982c00a4863a34e06b
jbodur <jbodur@team.amocrm.com>
2022-09-14 01:34:07 +0300

    arrow down icon

---
+++ src/presentation/components/icons/icons.tsx
@@ -0,0 +1,12 @@
+import {ArrowDown} from './arrow-down';
+
+enum IconsType {
+  "ARROW_DOWN",
+}
+
+export const Icons = (name:keyof typeof IconsType) => {
+  switch(name){
+    case 'ARROW_DOWN':
+      return <ArrowDown/>
+  }
+}
